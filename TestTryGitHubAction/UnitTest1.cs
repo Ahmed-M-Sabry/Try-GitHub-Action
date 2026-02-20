ffe3786291e11ef445e832fe710263dac5f6c99e
Ahmed Mohamed <a7med.mohamed.sabri@gmail.com>
2026-02-20 23:20:57 +0200

    Add project files.

---
+++ TestTryGitHubAction/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+using TryGitHubAction;
+using Program = TryGitHubAction.Program;
+
+namespace TestTryGitHubAction
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            int result = Program.Add(5, 7);
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void Add_NegativeAndPositive_ReturnsCorrect()
+        {
+            int result = Program.Add(-3, 8);
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ TryGitHubAction/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TryGitHubAction
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET!");
+            int result = Add(5, 7);
+            Console.WriteLine($"5 + 7 = {result}");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
